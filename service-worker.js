41071db29bfbc8e28e602471bca59f7fb53c6b94
Matthew Damron <mattdamron@msn.com>
2021-03-26 08:27:21 -0600

    added pwa

--- service-worker.js
+++ service-worker.js
@@ -1,71 +1,67 @@
-const APP_PREFIX = 'FoodFest-';
+const APP_PREFIX = 'FoodEvent-';     
 const VERSION = 'version_01';
-const CACHE_NAME = APP_PREFIX + VERSION;
-
+const CACHE_NAME = APP_PREFIX + VERSION
 const FILES_TO_CACHE = [
-    "./index.html",
-    "./events.html",
-    "./tickets.html",
-    "./schedule.html",
-    "./assets/css/style.css",
-    "./assets/css/bootstrap.css",
-    "./assets/css/tickets.css",
-    "./dist/app.bundle.js",
-    "./dist/events.bundle.js",
-    "./dist/tickets.bundle.js",
-    "./dist/schedule.bundle.js"
+  "./index.html",
+  "./events.html",
+  "./tickets.html",
+  "./schedule.html",
+  "./assets/css/style.css",
+  "./assets/css/bootstrap.css",
+  "./assets/css/tickets.css",
+  "./dist/app.bundle.js",
+  "./dist/events.bundle.js",
+  "./dist/tickets.bundle.js",
+  "./dist/schedule.bundle.js"
 ];
 
-// Install the service worker
-self.addEventListener('install', function (evt) {
-    evt.waitUntil(
-        caches.open(CACHE_NAME).then(cache => {
-            console.log('Your files ' + CACHE_NAME + ' were pre-cached successfully!');
-            return cache.addAll(FILES_TO_CACHE);
-        })
-    );
+// Respond with cached resources
+self.addEventListener('fetch', function (e) {
+  console.log('fetch request : ' + e.request.url)
+  e.respondWith(
+    caches.match(e.request).then(function (request) {
+      if (request) { // if cache is available, respond with cache
+        console.log('responding with cache : ' + e.request.url)
+        return request
+      } else {       // if there are no cache, try fetching request
+        console.log('file is not cached, fetching : ' + e.request.url)
+        return fetch(e.request)
+      }
 
-    // self.skipWaiting();
-});
+      // You can omit if/else for console.log & put one line below like this too.
+      // return request || fetch(e.request)
+    })
+  )
+})
 
-// Activate the service worker and remove old data from the cache
-self.addEventListener('activate', function (evt) {
-    evt.waitUntil(
-        caches.keys().then(keyList => {
-            let cacheKeeplist = keyList.filter(key => {
-                return key.indexOf(APP_PREFIX);
-            });
-            cacheKeeplist.push(CACHE_NAME);
+// Cache resources
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log('installing cache : ' + CACHE_NAME)
+      return cache.addAll(FILES_TO_CACHE)
+    })
+  )
+})
 
-            return Promise.all(
-                keyList.map(key, i => {
-                    if (cacheKeeplist.indexOf(key) === -1) {
-                        console.log('Removing old cache data' + keyList[i]);
-                        return caches.delete(key[i]);
-                    }
-                })
-            );
-        })
-    );
+// Delete outdated caches
+self.addEventListener('activate', function (e) {
+  e.waitUntil(
+    caches.keys().then(function (keyList) {
+      // `keyList` contains all cache names under your username.github.io
+      // filter out ones that has this app prefix to create keeplist
+      let cacheKeeplist = keyList.filter(function (key) {
+        return key.indexOf(APP_PREFIX);
+      })
+      // add current cache name to keeplist
+      cacheKeeplist.push(CACHE_NAME);
 
-    // self.clients.claim();
-});
-
-// Intercept fetch requests
-self.addEventListener('fetch', function (evt) {
-    console.log('fetch request : ' + evt.request.url);
-    evt.respondWith(
-        caches.match(evt.request).then(request => {
-            if (request) { // if cache is available, respond with cache
-                console.log('responding with cache : ' + evt.request.url);
-                return request;
-            } else { // if there are no cache, try fetching request
-                console.log('file is not cached, fetching : ' + evt.request.url);
-                return fetch(evt.request);
-            }
-
-            // You can omit if/else for console.log & put one line below like this too.
-            // return request || fetch(e.request)
-        })
-    );
+      return Promise.all(keyList.map(function (key, i) {
+        if (cacheKeeplist.indexOf(key) === -1) {
+          console.log('deleting cache : ' + keyList[i] );
+          return caches.delete(keyList[i]);
+        }
+      }));
+    })
+  );
 });
